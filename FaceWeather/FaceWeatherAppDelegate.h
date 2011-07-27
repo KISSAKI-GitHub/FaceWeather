d9ee1980b75f3a422c566d430f4a4f00d9112a0d
toru inoue <sassembla@mac.com>
2011-07-27 17:14:15 +0900

    Initial Commit

---
+++ FaceWeather/FaceWeatherAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FaceWeatherAppDelegate.h
+//  FaceWeather
+//
+//  Created by Toru Inoue on 11/07/27.
+//  Copyright 2011 KISSAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceWeatherAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
